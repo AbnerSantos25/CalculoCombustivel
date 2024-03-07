baaac51a5e9db139fa8d55bbf68315470d0ca560
abner <abysantos2509@gmail.com>
2024-03-07 20:10:17 -0300

    editando o tipo

--- Combustivel.cs
+++ Combustivel.cs
@@ -6,7 +6,7 @@
     public class Combustivel
     {
         public int Id { get; set; }
-        public TipoCombustivel TipoCombustivel { get; set; }
+        public TipoCombustivel Tipo{ get; set; }
         public double ValorGasolina { get; set; }
         public double ValorEtanol { get; set; }
 
